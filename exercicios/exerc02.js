57bf8e8969affe8b79a96b95fc85bfae5b619d04
John <jhonatansilva.1305@gmail.com>
2024-03-04 15:40:59 -0300

    exercicios

--- exercicios/exerc02.js
+++ exercicios/exerc02.js
@@ -1,4 +1,5 @@
-
+// Faça um programa que leia 2 números, 
+// some-os e exiba uma mensagem com o resultado apenas se o resultado for maior que 100.
 let imprimirResultado = function(a, b) {
     
     if (a + b > 100)
